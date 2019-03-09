0854809e95daddb5687471dd4ad2299ee3e61e68
Mahadev Shinde <mahadeva.shindhe@tavant.com>
2019-03-09 11:41:25 +0530

    list examples

--- basics/example-set1/Examples.py
+++ basics/example-set1/Examples.py
@@ -155,4 +155,68 @@ x = 5
 
 print(x)
 
+#7. Python lists
 
+# create a list
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+# Access list items
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1])
+
+# change the value of a list item
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+
+print(thislist)
+
+# Loop through a list
+thislist = ["apple", "banana", "cherry"]
+for x in thislist:
+  print(x)
+
+# check if a list item exists
+thislist = ["apple", "banana", "cherry"]
+if "apple" in thislist:
+  print("Yes, 'apple' is in the fruits list")
+  
+# Get the length of a list
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+
+# Add an item to the end of a list
+thislist = ["apple", "banana", "cherry"]
+
+thislist.append("orange")
+
+print(thislist)
+
+# Add an item at a specified index
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(1, "orange")
+print(thislist)
+
+# Remove an item
+thislist = ["apple", "banana", "cherry"]
+thislist.remove("banana")
+print(thislist)
+
+# Remove the last item
+thislist = ["apple", "banana", "cherry"]
+thislist.pop()
+print(thislist)
+
+# Remove an item at a specified index
+thislist = ["apple", "banana", "cherry"]
+del thislist[0]
+print(thislist)
+
+# Empty a list
+thislist = ["apple", "banana", "cherry"]
+thislist.clear()
+print(thislist)
+
+# Using the list() constructor to make a list
+thislist = list(("apple", "banana", "cherry"))
+print(thislist)
